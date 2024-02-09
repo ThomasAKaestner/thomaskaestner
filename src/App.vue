4e33d4f778482596f1ffb658b74f113c8c693276
Thomas Kaestner <thomaskaestner@Thomass-MBP.fritz.box>
2024-02-09 11:01:19 +0100

    adding new pages

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
     <div id="app">
         <router-view/>
-        <nav class="bg-black text-gray-200">
-            <router-link to="/">Hauptseite</router-link>
-            |
-            <router-link to="/portfolio">Portfolio</router-link>
-            |
-            <router-link to="/portfolio">Blog</router-link>
-            |
-            <router-link to="/impressum">Impressum</router-link>
-        </nav>
+        <div id="footer">
+            <footer class="bg-black">
+                <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+                <span class="text-sm text-gray-500 sm:text-center">© 2024 <a href="https://flowbite.com/"
+                                                                             class="hover:underline">Thomas Kästner</a>. All Rights Reserved.</span>
+                    <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500  sm:mt-0">
+                        <li>
+                            <a href="/" class="hover:underline me-4 md:me-6">Hauptseite</a>
+                        </li>
+                        <li>
+                            <a href="/blog" class="hover:underline me-4 md:me-6">Blog</a>
+                        </li>
+                        <li>
+                            <a href="/impressum" class="hover:underline me-4 md:me-6">Impressum</a>
+                        </li>
+                        <li>
+                            <a href="/#contact" class="hover:underline">Kontakt</a>
+                        </li>
+                    </ul>
+                </div>
+            </footer>
+
+        </div>
     </div>
 </template>
 
---
+++ src/views/BlogView.vue
@@ -0,0 +1,15 @@
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+    name: "BlogView"
+})
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style scoped>
+
+</style>
