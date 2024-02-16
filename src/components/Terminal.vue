4620c7773727a1707be20396069266f70033b94e
Thomas Kaestner <thomaskaestner@Thomass-MacBook-Pro.fritz.box>
2024-02-16 11:43:51 +0100

    blog/xdebug

---
+++ src/components/Terminal.vue
@@ -0,0 +1,33 @@
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+    name: "Terminal"
+})
+</script>
+
+<template>
+    <div id="terminal" class="py-2">
+        <div class="customThirdBackgroundColor text-white rounded-lg shadow-md">
+            <div class="bg-gray-600 rounded-t-lg px-4 py-2 mb-2 flex items-center">
+                <div class="w-3 h-3 bg-red-500 rounded-full mr-2"></div>
+                <div class="w-3 h-3 bg-yellow-500 rounded-full mr-2"></div>
+                <div class="w-3 h-3 bg-green-500 rounded-full mr-2"></div>
+            </div>
+            <div class="customThirdBackgroundColor mx-3 pb-1 text-black">
+                <p>
+                    <span class="text-black"></span>
+                    <pre><code class="text-sm">{{ code }}</code></pre>
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        code: String
+    }
+}
+</script>
--- src/views/articles/XdebugInDockerContainer.vue
+++ src/views/articles/XdebugInDockerContainer.vue
@@ -20,8 +20,8 @@
                     PHP-Anwendungen zu erleichtern.
                 </h4>
 
-
-                <div class="mb-4">
+                <!-- Neues Projekt anlegen -->
+                <div class="mb-6">
                     <h5 class="text-xl font-bold my-4">1. Neues Projekt anlegen:</h5>
                     <p class="">Erstellen Sie einen neuen Ordner für Ihr Projekt und navigieren Sie in
                         diesen Ordner.</p>
@@ -37,186 +37,198 @@
                     </div>
                 </div>
 
-                <div class="mb-4">
-                    <h5 class="text-xl font-bold my-4">2. Dockerfile anlegen:</h5>
-                    <p class="">Erstellen Sie eine Datei namens <code
-                        class="bg-gray-700 p-1">Dockerfile</code> im Projektordner und fügen Sie den
-                        entsprechenden Inhalt ein.</p>
-
-                    <div class="customThirdBackgroundColor text-white rounded-lg shadow-md">
-                        <div class="bg-gray-600 rounded-t-lg px-4 py-2 mb-2 flex items-center">
-                            <div class="w-3 h-3 bg-red-500 rounded-full mr-2"></div>
-                            <div class="w-3 h-3 bg-yellow-500 rounded-full mr-2"></div>
-                            <div class="w-3 h-3 bg-green-500 rounded-full mr-2"></div>
-                        </div>
-                        <div class="customThirdBackgroundColor mx-3 pb-1 text-black">
-                            <p>
-                                <span class="text-black"></span>
-                                <code class="text-sm">
-                                    FROM php:8.3-fpm
-                                    WORKDIR "/application"
-
-                                    RUN apt-get update \
-                                    && apt-get -y --no-install-recommends install \
-                                    php8.3-xdebug \
-                                    && apt-get clean \
-                                    && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* /usr/share/doc/*
-                                </code>
-                            </p>
-                        </div>
-                    </div>
+                <!-- Ordner Struktur anlegen -->
+                <div class="mb-6">
+                    <h5 class="text-xl font-bold my-4">2. Ordner Struktur anlegen:</h5>
+                    <p class="">Erstelle einen neuen Ordner im Hauptverzeichnis <code
+                            class="bg-gray-700 p-1">docker</code>
+                        und erstelle zwei neue Ordner in dem docker Ordner:</p>
+                    <code
+                            class="bg-gray-700 p-1">nginx</code>
+                    und
+                    <code
+                            class="bg-gray-700 p-1">php-fpm</code>
                 </div>
 
-
-                <div class="mb-4">
-                    <h5 class="text-xl font-bold my-4">3. docker-compose Datei anlegen:</h5>
+                <!-- Dockerfile anlegen -->
+                <div class="mb-6">
+                    <h5 class="text-xl font-bold my-4">3. Dockerfile anlegen:</h5>
                     <p class="">Erstellen Sie eine Datei namens <code
-                        class="bg-gray-700 p-1">docker-compose.yml</code> im Projektordner und fügen Sie den
+                            class="bg-gray-700 p-1">Dockerfile</code> im <code
+                            class="bg-gray-700 p-1">php-fpm</code> Ordner und fügen Sie den
                         entsprechenden Inhalt ein.</p>
-                    <div class="customThirdBackgroundColor text-white rounded-lg shadow-md">
-                        <div class="bg-gray-600 rounded-t-lg px-4 py-2 mb-2 flex items-center">
-                            <div class="w-3 h-3 bg-red-500 rounded-full mr-2"></div>
-                            <div class="w-3 h-3 bg-yellow-500 rounded-full mr-2"></div>
-                            <div class="w-3 h-3 bg-green-500 rounded-full mr-2"></div>
-                        </div>
-                        <div class="customThirdBackgroundColor mx-3 pb-1 text-black">
-                            <p>
-                                <span class="text-black"></span>
-                                <code class="text-sm">
-                                    version: '3.1'
-                                    services:
-                                    webserver:
-                                    image: 'nginx:alpine'
-                                    working_dir: /application
-                                    volumes:
-                                    - '.:/application'
-                                    - './docker/nginx/nginx.conf:/etc/nginx/conf.d/default.conf'
-                                    ports:
-                                    - '22000:80'
-
-                                    php-fpm:
-                                    environment:
-                                    PHP_IDE_CONFIG: "serverName=Docker"
-                                    build: php-fpm
-                                    working_dir: /application
-                                    volumes:
-                                    - '.:/application'
-                                    - './docker/php-fpm/php-ini-overrides.ini:/etc/php/8.3/fpm/conf.d/99-overrides.ini'
-                                </code>
-                            </p>
-                        </div>
+                    <Terminal :code="dockerfileCode"></Terminal>
+                    <div class="prose bg-black text-white">
+                        <p>Dieses Dockerfile definiert eine Docker-Image-Build-Anleitung für die Erstellung eines Images, das PHP 8.3 mit dem FPM (FastCGI Process Manager) als Basis verwendet.</p>
+                        <p>Hier ist eine Erklärung Zeile für Zeile:</p>
+                        <ol class="list-decimal list-inside ml-6">
+                            <li><span class="font-bold">FROM php:8.3-fpm</span>: Dies definiert das Basisimage für das Docker-Image. In diesem Fall wird das Image "php:8.3-fpm" verwendet, das eine vorinstallierte PHP 8.3-Version mit dem FPM-Manager bereitstellt.</li>
+                            <li><span class="font-bold">WORKDIR "/application"</span>: Diese Anweisung legt das Arbeitsverzeichnis im Container auf "/application" fest. Alle weiteren Befehle werden relativ dazu ausgeführt.</li>
+                            <li><span class="font-bold">RUN apt-get update \</span>: Aktualisiert die Paketliste im Container, um sicherzustellen, dass die neuesten Versionen verfügbar sind.</li>
+                            <li><span class="font-bold">&& apt-get -y --no-install-recommends install \ php8.3-xdebug \</span>: Installiert das PHP-Xdebug-Paket für die Version 8.3. Xdebug ist ein leistungsstarkes Debugging-Tool für PHP. Der Parameter <span class="italic">-y</span> stellt sicher, dass apt-get ohne manuelle Bestätigung ausgeführt wird, und <span class="italic">--no-install-recommends</span> installiert keine optionalen Pakete, was die Größe des Images reduziert.</li>
+                            <li><span class="font-bold">&& apt-get clean \</span>: Bereinigt das apt-Paket-Cache, um das Image zu verkleinern und ungenutzten Speicherplatz freizugeben.</li>
+                            <li><span class="font-bold">&& rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* /usr/share/doc/*</span>: Löscht temporäre Dateien und Verzeichnisse, um die Größe des Images weiter zu reduzieren und unnötige Dateien zu entfernen.</li>
+                        </ol>
+                        <p>Diese Dockerfile erstellt also ein Docker-Image, das PHP 8.3 mit Xdebug-Unterstützung basierend auf dem FPM-Manager enthält und dabei unnötige Dateien und temporäre Verzeichnisse entfernt, um die Image-Größe zu minimieren.</p>
                     </div>
+
                 </div>
 
-
-
+                <!-- php-ini-overrides.ini anlegen -->
                 <div class="mb-4">
-                    <h5 class="text-xl font-bold my-4">4. Nginx konfigurieren:</h5>
-                    <p class="">Erstellen Sie einen Ordner namens <code
-                        class="bg-gray-700 p-1">nginx</code> im Projektordner</p>
+                    <h5 class="text-xl font-bold my-4">5. php-ini-overrides.ini anlegen:</h5>
+                    <p>Lege nun im
+                        <code class="bg-gray-700 p-1">php-fpm</code>
+                        die <code class="bg-gray-700 p-1">php-ini-overrides.ini</code> datei an.
+                    </p>
+                    <Terminal :code="phpIniCode"></Terminal>
+                    <div class="prose bg-black text-white">
+                        <p><span class="italic">upload_max_filesize = 100M</span>: Dies legt die maximale Dateigröße fest, die hochgeladen werden kann. In diesem Fall ist die maximale Größe auf 100 Megabyte festgelegt.</p>
+                        <p><span class="italic">post_max_size = 108M</span>: Dies legt die maximale Größe für POST-Daten fest, die von PHP akzeptiert werden. In diesem Fall ist die maximale Größe auf 108 Megabyte festgelegt.</p>
+                        <p><span class="italic">xdebug.mode = debug</span>: Dies legt den Modus von Xdebug fest. In diesem Fall wird der Debug-Modus aktiviert.</p>
+                        <p><span class="italic">xdebug.client_host = host.docker.internal</span>: Dies legt die Hostadresse fest, die von Xdebug zum Verbinden mit einem Debugger verwendet wird. In diesem Fall wird "host.docker.internal" verwendet, um auf den Host zuzugreifen, auf dem Docker läuft.</p>
+                        <p><span class="italic">xdebug.start_with_request = yes</span>: Dies legt fest, ob Xdebug bei jeder Anfrage gestartet werden soll. In diesem Fall wird Xdebug gestartet, wenn eine Anfrage empfangen wird.</p>
+                    </div>
+
+                </div>
+
+                <!-- Nginx konfigurieren -->
+                <div class="mb-4">
+                    <h5 class="text-xl font-bold my-4">6. Nginx konfigurieren:</h5>
                     <p class="">Erstellen nun eine Datei <code
-                        class="bg-gray-700 p-1">nginx.conf</code> im nginx Ordner und füge folgenden Inhalt ein:</p>
+                            class="bg-gray-700 p-1">nginx.conf</code> im nginx Ordner und füge folgenden Inhalt ein:</p>
+                    <Terminal :code="nginxConfigCode"></Terminal>
+                </div>
 
-                    <div class="customThirdBackgroundColor text-white rounded-lg shadow-md">
-                        <div class="bg-gray-600 rounded-t-lg px-4 py-2 mb-2 flex items-center">
-                            <div class="w-3 h-3 bg-red-500 rounded-full mr-2"></div>
-                            <div class="w-3 h-3 bg-yellow-500 rounded-full mr-2"></div>
-                            <div class="w-3 h-3 bg-green-500 rounded-full mr-2"></div>
-                        </div>
-                        <div class="customThirdBackgroundColor mx-3 pb-1 text-black">
-                            <p>
-                                <span class="text-black"></span>
-                                <code class="text-sm">
-                                    server {
-                                    listen 80 default;
+                <!-- docker-compose Datei anlegen -->
+                <div class="mb-4">
+                    <h5 class="text-xl font-bold my-4">7. docker-compose Datei anlegen:</h5>
+                    <p class="">Erstellen Sie eine Datei namens <code
+                            class="bg-gray-700 p-1">docker-compose.yml</code> im Projektordner und fügen Sie den
+                        entsprechenden Inhalt ein.</p>
 
-                                    client_max_body_size 108M;
-
-                                    access_log /var/log/nginx/application.access.log;
-
-                                    root /application;
-                                    index index.php;
-
-                                    # try to serve file directly, fallback to index.php
-                                    location / {
-                                    try_files $uri /index.php$is_args$args;
-                                    }
-
-                                    if (!-e $request_filename) {
-                                    rewrite ^.*$ /index.php last;
-                                    }
-
-                                    location ~ \.php$ {
-                                    fastcgi_pass php-fpm:9000;
-                                    fastcgi_index index.php;
-                                    fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-                                    fastcgi_param PHP_VALUE "error_log=/var/log/nginx/application_php_errors.log";
-                                    fastcgi_buffers 16 16k;
-                                    fastcgi_buffer_size 32k;
-                                    include fastcgi_params;
-                                    }
-                                    }
-
-                                </code>
-                            </p>
-                        </div>
-                    </div>
-
-
+                    <Terminal :code="dockerComposeCode"></Terminal>
                 </div>
 
 
                 <div class="mb-4">
-                    <h5 class="text-xl font-bold my-4">5. Index.php anlegen:</h5>
+                    <h5 class="text-xl font-bold my-4">5. Index.php zum testen anlegen:</h5>
                     <p class="">Erstellen Sie eine Datei namens <code
-                        class="bg-gray-700 p-1">index.php</code> im Projektordner und fügen Sie den
+                            class="bg-gray-700 p-1">index.php</code> im Projektordner und fügen Sie den
                         entsprechenden Inhalt ein.</p>
-
-                    <div class="customThirdBackgroundColor text-white rounded-lg shadow-md">
-                        <div class="bg-gray-600 rounded-t-lg px-4 py-2 mb-2 flex items-center">
-                            <div class="w-3 h-3 bg-red-500 rounded-full mr-2"></div>
-                            <div class="w-3 h-3 bg-yellow-500 rounded-full mr-2"></div>
-                            <div class="w-3 h-3 bg-green-500 rounded-full mr-2"></div>
-                        </div>
-                        <div class="customThirdBackgroundColor mx-3 pb-1 text-black">
-                            <p>
-                                <span class="text-black"></span>
-                                <code class="text-sm">
-                                        ""
-                                </code>
-                            </p>
-                        </div>
-                    </div>
-
+                    <Terminal :code="indexCode"></Terminal>
                 </div>
 
 
                 <div class="mb-4">
                     <h5 class="text-xl font-bold my-4">6. Debug Settings in PHPStorm einrichten</h5>
-                    <p class="">Erstellen Sie eine Datei namens <code
-                        class="bg-gray-700 p-1">Dockerfile</code> im Projektordner und fügen Sie den
-                        entsprechenden Inhalt ein.</p>
+                    <p>
+                        Zum test müssen wir in den PHPStorm Settings unter Server einen neuen Server mit dem Namen  <code
+                        class="bg-gray-700 p-1">Docker</code> anlegen. Host ist hier 0.0.0.0 und port 2000. Als debugger muss Xdebug ausgewählt werden
+                    </p>
+                    <p>
+                        Beim pathmapping muss hier an die index.php mit /application/index.php gemappet werden.
+                    </p>
+                    <img src="/img/screenshots/xdebugSetting.png" class="w-full">
                 </div>
-
 
 
                 <div class="mb-4">
                     <h5 class="text-xl font-bold my-4">7. Testen</h5>
                     <p class="">Erstellen Sie eine Datei namens <code
-                        class="bg-gray-700 p-1">Dockerfile</code> im Projektordner und fügen Sie den
+                            class="bg-gray-700 p-1">Dockerfile</code> im Projektordner und fügen Sie den
                         entsprechenden Inhalt ein.</p>
-
-
-
                 </div>
-
-
-
             </div>
         </div>
     </div>
 </template>
 
-<style scoped>
+<script>
+import Terminal from "@/components/Terminal.vue";
 
-</style>
+export default {
+    components: {
+        Terminal
+    },
+    data() {
+        return {
+            dockerfileCode: `
+FROM php:8.3-fpm
+WORKDIR "/application"
+
+RUN apt-get update \\
+&& apt-get -y --no-install-recommends install \\
+php8.3-xdebug \\
+&& apt-get clean \\
+&& rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* /usr/share/doc/*`,
+            dockerComposeCode: `
+version: '3.1'
+services:
+webserver:
+image: 'nginx:alpine'
+working_dir: /application
+volumes:
+- '.:/application'
+- './docker/nginx/nginx.conf:/etc/nginx/conf.d/default.conf'
+ports:
+- '22000:80'
+
+php-fpm:
+environment:
+PHP_IDE_CONFIG: "serverName=Docker"
+build: php-fpm
+working_dir: /application
+volumes:
+- '.:/application'
+- './docker/php-fpm/php-ini-overrides.ini:/etc/php/8.3/fpm/conf.d/99-overrides.ini'
+`,
+            nginxConfigCode: `
+server {
+    listen 80 default;
+
+    client_max_body_size 108M;
+
+    access_log /var/log/nginx/application.access.log;
+
+    root /application;
+    index index.php;
+
+    # try to serve file directly, fallback to index.php
+    location / {
+        try_files $uri /index.php$is_args$args;
+    }
+
+    if (!-e $request_filename) {
+        rewrite ^.*$ /index.php last;
+    }
+
+    location ~ \\.php$ {
+        fastcgi_pass php-fpm:9000;
+        fastcgi_index index.php;
+        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+        fastcgi_param PHP_VALUE "error_log=/var/log/nginx/application_php_errors.log";
+        fastcgi_buffers 16 16k;
+        fastcgi_buffer_size 32k;
+        include fastcgi_params;
+    }
+}`,
+            phpIniCode: `
+upload_max_filesize = 100M
+post_max_size = 108M
+xdebug.mode = debug
+xdebug.client_host = host.docker.internal
+xdebug.start_with_request = yes`, indexCode: `
+<?php
+
+$var = "a";
+
+$var = $var . "b";
+
+echo $var;
+return $var;`
+        }
+    }
+};
+
+</script>
